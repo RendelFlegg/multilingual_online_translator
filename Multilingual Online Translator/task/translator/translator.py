03c76014784a55d589bc24cb32b7e66989f5597e
RendelFlegg <rendelflegg@ya.ru>
2022-05-31 12:59:44 +0300

    Completed Stage 5: Implemented multilanguage translation and saving to files

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -6,16 +6,21 @@ def get_language():
     languages = {'1': 'arabic', '2': 'german', '3': 'english', '4': 'spanish', '5': 'french', '6': 'hebrew',
                  '7': 'japanese', '8': 'dutch', '9': 'polish', '10': 'portuguese', '11': 'romanian',
                  '12': 'russian', '13': 'turkish'}
+    translated_languages = []
     print('Hello, welcome to the translator. Translator supports: ')
     for key, value in languages.items():
         print(f'{key}. {value.capitalize()}')
-    number_1 = input('Type the number of your language: \n')
-    while number_1 not in languages:
-        number_1 = input('Type the number of your language: \n')
-    number_2 = input('Type the number of language you want to translate to: \n')
-    while number_2 not in languages:
-        number_2 = input('Type the number of language you want to translate to: \n')
-    return languages[number_1], languages[number_2]
+    user_language = input('Type the number of your language: \n')
+    while user_language not in languages:
+        user_language = input('Type the number of your language: \n')
+    user_number = input('Type the number of language you want to translate to: \n')
+    while user_number not in languages and user_number != '0':
+        user_number = input('Type the number of language you want to translate to: \n')
+    if user_number == '0':
+        translated_languages = [languages[number] for number in languages if number != user_language]
+    else:
+        translated_languages.append(languages[user_number])
+    return languages[user_language], translated_languages
 
 
 def get_word():
@@ -68,16 +73,63 @@ def print_examples(language, examples):
         print()
 
 
+def print_dictionary(dictionary):
+    for language in dictionary:
+        print_translations(language, dictionary[language]['translations'])
+        print_examples(language, dictionary[language]['examples'])
+
+
+def update_dictionary(dictionary, user_language, translated_languages, word):
+    multilanguage = len(translated_languages) > 1
+    for language in translated_languages:
+        url = get_url(user_language, language, word)
+        page = get_page(url)
+        translations = get_translations(page)
+        examples = get_examples(page)
+        if multilanguage:
+            translations = translations[:1]
+            examples = examples[:2]
+        dictionary[language] = {'translations': translations, 'examples': examples}
+
+
+def write_translations(word, language, translations):
+    with open(f'{word}.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{language.capitalize()} Translations:\n')
+        for translation in translations:
+            f.write(f'{translation}\n')
+        f.write('\n')
+
+
+def write_examples(word, language, examples):
+    examples_tuples = zip(*[iter(examples)]*2)
+    with open(f'{word}.txt', 'a', encoding='utf-8') as f:
+        f.write(f'\n{language.capitalize()} Examples:\n')
+        for example in examples_tuples:
+            f.write(f'{example[0]}:\n')
+            f.write(f'{example[1]}\n')
+            f.write('\n')
+
+
+def write_file(dictionary, word):
+    for language in dictionary:
+        write_translations(word, language, dictionary[language]['translations'])
+        write_examples(word, language, dictionary[language]['examples'])
+
+
+def read_file(word):
+    with open(f'{word}.txt', 'r', encoding='utf-8') as f:
+        print(f.read())
+
+
 def translator():
-    language_1, language_2 = get_language()
+    dictionary = {}
+    user_language, translated_languages = get_language()
     word = get_word()
-    url = get_url(language_1, language_2, word)
-    page = get_page(url)
-    translations = get_translations(page)
-    examples = get_examples(page)
-    print_translations(language_2, translations)
-    print_examples(language_2, examples)
+    update_dictionary(dictionary, user_language, translated_languages, word)
+    write_file(dictionary, word)
+    read_file(word)
 
 
 if __name__ == '__main__':
     translator()
+
